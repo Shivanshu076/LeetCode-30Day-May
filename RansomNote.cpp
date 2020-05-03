90abf013709fda20c5f3c4c75337b8255fed793d
Shivanshu076 <38668518+Shivanshu076@users.noreply.github.com>
2020-05-03 14:30:11 +0530

    Update RansomNote.cpp

--- RansomNote.cpp
+++ RansomNote.cpp
@@ -7,10 +7,10 @@ Each letter in the magazine string can only be used once in your ransom note.
 // Solution using hashing : 
 
 bool canConstruct(string ransomNote, string magazine) {
-        int ak[300]={};
-        int bk[300]={};
-        for(int i=0;i<ransomNote.length();i++) ak[ransomNote[i]]++;
-        for(int i=0;i<magazine.length();i++) bk[magazine[i]]++;
-        for(int i=0;i<300;i++) if(ak[i]>bk[i]) return 0;
-        return 1;
-    }
+     int ak[300]={};
+     int bk[300]={};
+     for(int i=0;i<ransomNote.length();i++) ak[ransomNote[i]]++;
+     for(int i=0;i<magazine.length();i++) bk[magazine[i]]++;
+     for(int i=0;i<300;i++) if(ak[i]>bk[i]) return 0;
+     return 1;
+}
