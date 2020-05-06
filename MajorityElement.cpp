6f3ac5347dcdd753ab808c6ed6c0ba887e140d7b
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-06 14:45:26 +0530

    Update MajorityElement.cpp

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -7,23 +7,23 @@ You may assume that the array is non-empty and the majority element always exist
 // Solution using hashing :
 
 int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
-            if(mp.find(nums[i])==mp.end()) mp[nums[i]]=1;
-            else mp[nums[i]]++;
-            if(mp[nums[i]] > nums.size()/2) return nums[i];
-        }
-        return -1;
+    unordered_map<int,int> mp;
+    for(int i=0;i<nums.size();i++){
+        if(mp.find(nums[i])==mp.end()) mp[nums[i]]=1;
+        else mp[nums[i]]++;
+        if(mp[nums[i]] > nums.size()/2) return nums[i];
     }
+    return -1;
+}
     
 // Solution using Boyer-Moore Voting Algorithm :
 
 int majorityElement(vector<int>& nums) {
-        int a, c=0;
-        for(int i=0;i<nums.size();i++){
-            if(!c) a=nums[i];
-            if(nums[i]==a) c++;
-            else c--;
-        }
-        return a;
+    int a, c=0;
+    for(int i=0;i<nums.size();i++){
+        if(!c) a=nums[i];
+        if(nums[i]==a) c++;
+        else c--;
     }
+    return a;
+}
