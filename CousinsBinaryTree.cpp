3da951df997b31b3abd31a096538947e40c67e07
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-07 17:17:52 +0530

    Update CousinsBinaryTree.cpp

--- CousinsBinaryTree.cpp
+++ CousinsBinaryTree.cpp
@@ -12,20 +12,20 @@ Return true if and only if the nodes corresponding to the values x and y are cou
 
 typedef pair<int,int> pi;
     
-    void fn(TreeNode* n,int t, int cd, int p, pi &ans){
-        if(!n) return;
-        if(n->val==t){
-            ans= make_pair(cd,p);
-            return;
-        }
-        fn(n->left,t,cd+1,n->val,ans);
-        fn(n->right,t,cd+1,n->val,ans);
+void fn(TreeNode* n,int t, int cd, int p, pi &ans){
+    if(!n) return;
+    if(n->val==t){
+        ans= make_pair(cd,p);
+        return;
     }
+    fn(n->left,t,cd+1,n->val,ans);
+    fn(n->right,t,cd+1,n->val,ans);
+}
     
-    bool isCousins(TreeNode* root, int x, int y) {
-        pi xx,yy;
-        fn(root,x,0,-1,xx);
-        fn(root,y,0,-1,yy);
-        if(xx.first==yy.first && xx.second!=yy.second) return 1;
-        return 0;
-    }
+bool isCousins(TreeNode* root, int x, int y) {
+    pi xx,yy;
+    fn(root,x,0,-1,xx);
+    fn(root,y,0,-1,yy);
+    if(xx.first==yy.first && xx.second!=yy.second) return 1;
+    return 0;
+}
