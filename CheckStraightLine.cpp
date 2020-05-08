bf26e23cd201b39f2e5024e39e3349ddb1810cb9
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-08 16:10:09 +0530

    Update CheckStraightLine.cpp

--- CheckStraightLine.cpp
+++ CheckStraightLine.cpp
@@ -3,8 +3,8 @@
 // Solution :
 
 bool checkStraightLine(vector<vector<int>>& coordinates) {
-        double a = (double(coordinates[1][1]-coordinates[0][1])/double(coordinates[1][0]-coordinates[0][0]) );
-        for(int i=2;i<coordinates.size();i++) 
-            if(double(double(coordinates[i][1]-coordinates[i-1][1])/double(coordinates[i][0]-coordinates[i-1][0])) != a) return 0;
-        return 1;
-    }
+    double a = (double(coordinates[1][1]-coordinates[0][1])/double(coordinates[1][0]-coordinates[0][0]) );
+    for(int i=2;i<coordinates.size();i++) 
+        if(double(double(coordinates[i][1]-coordinates[i-1][1])/double(coordinates[i][0]-coordinates[i-1][0])) != a) return 0;
+    return 1;
+}
