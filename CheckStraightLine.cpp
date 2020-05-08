808146d302365a0cbcce2ea22c48e9ebaa95e54d
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-08 16:09:41 +0530

    Create CheckStraightLine.cpp

---
+++ CheckStraightLine.cpp
@@ -0,0 +1,10 @@
+// You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+// Solution :
+
+bool checkStraightLine(vector<vector<int>>& coordinates) {
+        double a = (double(coordinates[1][1]-coordinates[0][1])/double(coordinates[1][0]-coordinates[0][0]) );
+        for(int i=2;i<coordinates.size();i++) 
+            if(double(double(coordinates[i][1]-coordinates[i-1][1])/double(coordinates[i][0]-coordinates[i-1][0])) != a) return 0;
+        return 1;
+    }
