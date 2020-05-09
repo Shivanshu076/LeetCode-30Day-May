1feb3e1ba09e73f1dac7164fa2d1722338ce731f
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-09 19:18:27 +0530

    Update ValidPerfectSquare.cpp

--- ValidPerfectSquare.cpp
+++ ValidPerfectSquare.cpp
@@ -7,16 +7,15 @@ Note: Do not use any built-in library function such as sqrt.
 // Solution (Binary Search) :
 
 bool isPerfectSquare(int num) {
-        if(num == 1) return true;
-        int l=0, r = num/2;
-        while(l <= r){
-            double mid = l + (r-l)/2;
-            //cout << mid << endl;
-            if(mid==num/mid) return true;
-            if(mid> num/mid){
-                r = mid-1;
-            }
-            else l = mid+1;
+    if(num == 1) return 1;
+    int l=0, r = num/2;
+    while(l <= r){
+        double mid = l + (r-l)/2;
+        if(mid==num/mid) return 1;
+        if(mid> num/mid){
+            r = mid-1;
         }
-        return false;
+        else l = mid+1;
     }
+    return 0;
+}
