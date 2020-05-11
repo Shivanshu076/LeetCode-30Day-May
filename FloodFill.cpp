247ff2d97970c61f921dd87c12c090878a4d0cba
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-11 20:52:40 +0530

    Update FloodFill.cpp

--- FloodFill.cpp
+++ FloodFill.cpp
@@ -11,23 +11,23 @@ At the end, return the modified image.
 // Solution using BFS :
 
 vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int x= image[sr][sc];
-        queue<pair<int,int>> q;
-        q.push(make_pair(sr,sc));
-        int a[4][2]= {{-1,0},{1,0},{0,-1},{0,1}};
-        bool vis[image.size()][image[0].size()];
-        for(int i=0;i<image.size();i++) for(int j=0;j<image[0].size();j++) vis[i][j]=0;
-        
-        while(!q.empty()){
-            pair<int,int> p=q.front();
-            q.pop();
-            image[p.first][p.second]=newColor;
-            vis[p.first][p.second]=1;
-            for(int i=0;i<4;i++){
-                int r= p.first+a[i][0];
-                int c= p.second+a[i][1];
-                if(r>=0 && r<image.size() && c>=0 && c<image[0].size() && !vis[r][c] && image[r][c]==x) q.push(make_pair(r,c));
-            }
+    int x= image[sr][sc];
+    queue<pair<int,int>> q;
+    q.push(make_pair(sr,sc));
+    int a[4][2]= {{-1,0},{1,0},{0,-1},{0,1}};
+    bool vis[image.size()][image[0].size()];
+    for(int i=0;i<image.size();i++) for(int j=0;j<image[0].size();j++) vis[i][j]=0;
+    
+    while(!q.empty()){
+        pair<int,int> p=q.front();
+        q.pop();
+        image[p.first][p.second]=newColor;
+        vis[p.first][p.second]=1;
+        for(int i=0;i<4;i++){
+            int r= p.first+a[i][0];
+            int c= p.second+a[i][1];
+            if(r>=0 && r<image.size() && c>=0 && c<image[0].size() && !vis[r][c] && image[r][c]==x) q.push(make_pair(r,c));
         }
-        return image;
     }
+    return image;
+}
