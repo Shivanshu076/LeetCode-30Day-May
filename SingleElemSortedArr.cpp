77055736c53b4c9102c17a5a80607cac0ab925d5
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-12 21:31:04 +0530

    Update SingleElemSortedArr.cpp

--- SingleElemSortedArr.cpp
+++ SingleElemSortedArr.cpp
@@ -6,28 +6,26 @@ Your solution should run in O(log n) time and O(1) space.
 //Solution using Binary search :
 
 int singleNonDuplicate(vector<int>& nums) {
-        int l=0, h=nums.size()-1;
-        if(!h) return nums[h];
-        while(l<=h){
-            int m=(l+h)/2;
-            // cout<<l<<" "<<m<<" "<<h<<endl;
-            if(!(m%2)){
-                if(nums[m]==nums[m+1]){
-                    l=m+1;
-                }
-                else{
-                    h=m-1;
-                }
+    int l=0, h=nums.size()-1;
+    if(!h) return nums[h];
+    while(l<=h){
+        int m=(l+h)/2;
+        if(!(m%2)){
+            if(nums[m]==nums[m+1]){
+                l=m+1;
             }
             else{
-                if(nums[m]==nums[m+1]){
-                    h=m-1;
-                }
-                else{
-                    l=m+1;
-                }
+                h=m-1;
             }
         }
-        // cout<<l<<" "<<h<<endl;
-        return nums[l];
+        else{
+            if(nums[m]==nums[m+1]){
+                h=m-1;
+            }
+            else{
+                l=m+1;
+            }
+        }
     }
+    return nums[l];
+}
