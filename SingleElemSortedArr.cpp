bb56356202dcecfb702640faf0bc656d90caa729
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-12 21:30:00 +0530

    Create SingleElemSortedArr.cpp

---
+++ SingleElemSortedArr.cpp
@@ -0,0 +1,33 @@
+/*
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
+Your solution should run in O(log n) time and O(1) space.
+*/
+
+//Solution using Binary search :
+
+int singleNonDuplicate(vector<int>& nums) {
+        int l=0, h=nums.size()-1;
+        if(!h) return nums[h];
+        while(l<=h){
+            int m=(l+h)/2;
+            // cout<<l<<" "<<m<<" "<<h<<endl;
+            if(!(m%2)){
+                if(nums[m]==nums[m+1]){
+                    l=m+1;
+                }
+                else{
+                    h=m-1;
+                }
+            }
+            else{
+                if(nums[m]==nums[m+1]){
+                    h=m-1;
+                }
+                else{
+                    l=m+1;
+                }
+            }
+        }
+        // cout<<l<<" "<<h<<endl;
+        return nums[l];
+    }
