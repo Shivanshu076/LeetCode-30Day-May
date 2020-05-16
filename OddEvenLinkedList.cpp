3a990174deb1e764b5ca6d73fce62186d107ab0f
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-16 21:09:54 +0530

    Update OddEvenLinkedList.cpp

--- OddEvenLinkedList.cpp
+++ OddEvenLinkedList.cpp
@@ -7,20 +7,20 @@ You should try to do it in place. The program should run in O(1) space complexit
 //Solution :
 
 ListNode* oddEvenList(ListNode* head) {
-        if(!head) return head;
-        ListNode *a=head, *b=head->next, *c=head->next;
-        
-        while(a && b){
-            if(b->next){
-                a->next= b->next;
-                a=a->next;
-                if(a){
-                    b->next=a->next;
-                    b=b->next;
-                }
+    if(!head) return head;
+    ListNode *a=head, *b=head->next, *c=head->next;
+    
+    while(a && b){
+        if(b->next){
+            a->next= b->next;
+            a=a->next;
+            if(a){
+                b->next=a->next;
+                b=b->next;
             }
-            else break;
         }
-        a->next=c;
-        return head;
+        else break;
     }
+    a->next=c;
+    return head;
+}
