5e91a04ba296e1c077dbab740020baff1da6c990
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-22 17:25:11 +0530

    Create SortByCharFreq.cpp

---
+++ SortByCharFreq.cpp
@@ -0,0 +1,26 @@
+// Given a string, sort it in decreasing order based on the frequency of characters.
+
+// Solution using hashing and vector sorting :
+
+static bool myc(pair<char,int> a, pair<char,int> b){
+        return a.second>b.second;
+    }
+    
+    string frequencySort(string s) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.length();i++){
+            if(mp.find(s[i])==mp.end()) mp[s[i]]=1;
+            else mp[s[i]]++;
+        }
+        vector<pair<char,int>> v;
+        for(auto i=mp.begin(); i!=mp.end(); i++){
+            v.push_back(*i);
+        }
+        sort(v.begin(),v.end(),myc);
+        // for(int i=0;i<v.size();i++) cout<<v[i].first<<" "<<v[i].second<<endl;
+        string ss="";
+        for(int i=0;i<v.size();i++){
+            while(v[i].second--) ss+=v[i].first;
+        }
+        return ss;
+    }
