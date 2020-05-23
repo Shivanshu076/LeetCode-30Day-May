d6b84f823eccf0ee0e19a2802f55995e8de64675
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-23 22:08:37 +0530

    Update IntervalIntersections.cpp

--- IntervalIntersections.cpp
+++ IntervalIntersections.cpp
@@ -9,18 +9,18 @@ Return the intersection of these two interval lists.
 // Solution :
 
 vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
-        vector<vector<int>> ans;
-        int i=0, j=0;
-        while( i<A.size() && j<B.size() ){
-            vector<int> v;
-            int a= max(A[i][0] , B[j][0]);
-            int b= min(A[i][1] , B[j][1]);
-            if(a<=b){
-                v.push_back(a); v.push_back(b);
-                ans.push_back(v);
-            }
-            if(b==A[i][1]) i++;
-            else j++;
+    vector<vector<int>> ans;
+    int i=0, j=0;
+    while( i<A.size() && j<B.size() ){
+        vector<int> v;
+        int a= max(A[i][0] , B[j][0]);
+        int b= min(A[i][1] , B[j][1]);
+        if(a<=b){
+            v.push_back(a); v.push_back(b);
+            ans.push_back(v);
         }
-        return ans;
+        if(b==A[i][1]) i++;
+        else j++;
     }
+    return ans;
+}
