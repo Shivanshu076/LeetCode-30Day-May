e1507c5625347573fab225587c25b210d401fbe1
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-26 15:03:09 +0530

    Update ContiguousArray.cpp

--- ContiguousArray.cpp
+++ ContiguousArray.cpp
@@ -3,20 +3,17 @@
 // Solution using hashing :
 
 int findMaxLength(vector<int>& nums) {
-        int cnt = 0;
-        unordered_map<int,int> m;
-        unordered_map<int,int>::iterator it;
-        m[0] = -1;
-        int max_len = INT_MIN;
-        for(int i=0;i<nums.size();i+=1) {
-            cnt+= nums[i]?1:-1;
-            it = m.find(cnt);
-            if(it!=m.end()) {
-                max_len = max(max_len, i - it->second);
-            }else {
-                m.insert(make_pair(cnt,i));
-            }
-       }
-       if(max_len == INT_MIN) return 0;
-       else return max_len;
-    }
+    int cnt = 0;
+    unordered_map<int,int> m;
+    unordered_map<int,int>::iterator it;
+    m[0] = -1;
+    int max_len = INT_MIN;
+    for(int i=0;i<nums.size();i+=1) {
+        cnt+= nums[i]?1:-1;
+        it = m.find(cnt);
+        if(it!=m.end()) max_len = max(max_len, i - it->second);
+        else m.insert(make_pair(cnt,i));
+   }
+   if(max_len == INT_MIN) return 0;
+   else return max_len;
+}
