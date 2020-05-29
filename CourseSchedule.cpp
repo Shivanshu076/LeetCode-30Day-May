b1125c6309f1c7226001256cf3a7cb83bb7f2eb9
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-29 21:24:59 +0530

    Update CourseSchedule.cpp

--- CourseSchedule.cpp
+++ CourseSchedule.cpp
@@ -10,30 +10,28 @@ Given the total number of courses and a list of prerequisite pairs, is it possib
 // Cycle detection in a directed graph using DFS :
 
 void dfs(vector<int> adj[], bool vis[], int c, bool &flag, int par[], int o){
-        if(!flag) return;
-        vis[c]=1;
-        par[c]=o;
-        for(int i=0;i<adj[c].size();i++){
-            if(!vis[adj[c][i]]) dfs(adj,vis,adj[c][i], flag, par, o);
-            else if(par[adj[c][i]]==o && c!=o){
-                flag=0;
-                return;
-            }
+    if(!flag) return;
+    vis[c]=1;
+    par[c]=o;
+    for(int i=0;i<adj[c].size();i++){
+        if(!vis[adj[c][i]]) dfs(adj,vis,adj[c][i], flag, par, o);
+        else if(par[adj[c][i]]==o && c!=o){
+            flag=0;
+            return;
         }
-        par[c]=-1;
     }
+    par[c]=-1;
+}
     
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
-        for(int i=0;i<prerequisites.size();i++) (adj[prerequisites[i][0]]).push_back(prerequisites[i][1]);
-        bool vis[numCourses];
-        int par[numCourses];
-        memset(vis,0,sizeof(vis));
-        bool flag=1;
-        for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                dfs(adj,vis,i, flag, par, i);
-            }
-        }
-        return flag;
+bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> adj[numCourses];
+    for(int i=0;i<prerequisites.size();i++) (adj[prerequisites[i][0]]).push_back(prerequisites[i][1]);
+    bool vis[numCourses];
+    int par[numCourses];
+    memset(vis,0,sizeof(vis));
+    bool flag=1;
+    for(int i=0;i<numCourses;i++){
+        if(!vis[i]) dfs(adj,vis,i, flag, par, i);
     }
+    return flag;
+}
