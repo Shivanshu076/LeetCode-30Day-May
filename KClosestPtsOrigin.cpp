ba42fe51464559f9a4d41bb9ccdb62b4931868f6
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-30 19:56:51 +0530

    Update KClosestPtsOrigin.cpp

--- KClosestPtsOrigin.cpp
+++ KClosestPtsOrigin.cpp
@@ -9,28 +9,28 @@ You may return the answer in any order.  The answer is guaranteed to be unique (
 // Solution using custom class and sorting :
 
 class tt{
-    public:
-        int x,y;
-        double d;
-    };
-    static bool mc(tt a, tt b){
-        return a.d <= b.d;
+public:
+    int x,y;
+    double d;
+};
+
+static bool mc(tt a, tt b) return a.d <= b.d;
+
+vector<vector<int>> kClosest(vector<vector<int>>& p, int k) {
+    vector<vector<int>> ans;
+    vector<tt> v;
+    for(int i=0;i<p.size();i++){
+        tt t;
+        t.x= p[i][0];
+        t.y= p[i][1];
+        t.d= (double)sqrt((double)((double)p[i][0]*(double)p[i][0] + (double)p[i][1]*(double)p[i][1]));
+        v.push_back(t);
     }
-    vector<vector<int>> kClosest(vector<vector<int>>& p, int k) {
-        vector<vector<int>> ans;
-        vector<tt> v;
-        for(int i=0;i<p.size();i++){
-            tt t;
-            t.x= p[i][0];
-            t.y= p[i][1];
-            t.d= (double)sqrt((double)((double)p[i][0]*(double)p[i][0] + (double)p[i][1]*(double)p[i][1]));
-            v.push_back(t);
-        }
-        sort(v.begin(),v.end(),mc);
-        for(int i=0;i<k;i++){
-            vector<int> j;
-            j.push_back(v[i].x); j.push_back(v[i].y);
-            ans.push_back(j);
-        }
-        return ans;
+    sort(v.begin(),v.end(),mc);
+    for(int i=0;i<k;i++){
+        vector<int> j;
+        j.push_back(v[i].x); j.push_back(v[i].y);
+        ans.push_back(j);
     }
+    return ans;
+}
