cfb4fdc78f0872f11354238fd1adf69f5809c917
Shivanshu <38668518+Shivanshu076@users.noreply.github.com>
2020-05-31 21:07:22 +0530

    Update EditDistance.cpp

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -11,21 +11,21 @@ Replace a character
 // Solution using Dynamic Programming :
 
 int minDistance(string s1, string s2) {
-        int n=s1.length(), m=s2.length();
-        int dp[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                if(!i) dp[i][j]=j;
-                else if(!j) dp[i][j]=i;
+    int n=s1.length(), m=s2.length();
+    int dp[n+1][m+1];
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=m;j++){
+            if(!i) dp[i][j]=j;
+            else if(!j) dp[i][j]=i;
+            else{
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]= dp[i-1][j-1];
+                }
                 else{
-                    if(s1[i-1]==s2[j-1]){
-                        dp[i][j]= dp[i-1][j-1];
-                    }
-                    else{
-                        dp[i][j]= 1 + min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
-                    }
+                    dp[i][j]= 1 + min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
                 }
             }
         }
-        return dp[n][m];
     }
+    return dp[n][m];
+}
